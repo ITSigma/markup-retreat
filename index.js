92d0212b9f58521a37f8a3a64a5c50170c4a2fe7
ITSigma <ascoolasanton@gmail.com>
2024-04-09 19:04:22 +0500

    Stars ok

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
 
-const sList = document.querySelector(".s")
-"mouseover", "mouseout"
+const sList = document.getElementsByClassName("s")
+
 console.log(sList);
 
-over = new function (el) {
-    console.log(el);
-    sList[0].src = "img/star.png"
-    // const index = el.id
-    // for (let i = 1; i <= index; i++){
-    //     sList[i - 1].src = "img/star.png"
-    // }
-}
-
-out = new function (el) {
-    const index = el.id
-    for (let i = 1; i <= index; i++){
-        sList[i - 1].src = "img/star-empty.png"
+over = function (index) {
+    for (let i = 0; i <= index; i++) {
+        sList[i].style.visibility = "visible";
     }
 }
 
-sList[0].addEventListener("click", over);
+out = function () {
+    for (let i= 0; i < 5; i++){
+        sList[i].style.visibility = "hidden";
+    }
+}
